0837ed3192821d313823a382d79f9af7918fc092
Abby <ada77@drexel.edu>
2023-11-27 00:21:14 -0500

    week 8 forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,40 @@
 var mainNav = document.querySelector("#navv");
+var signInContainer = document.querySelector("#login");
 
-var hamburger = document.querySelector("#menu-container button");
+var burgerBtn = document.querySelector("#menu-container button");
 var visible = false;
 
+var canYouSeeMainNav = false;
+
 function showNav(){
-    console.log("hi");
-    if(visible == true){
-        mainNav.classList.remove("showMainNav");
-        visible = false
-    } else {
+    // console.log("button clicked");
+    if(canYouSeeMainNav === false){
+        console.log("show menu");
+        // this will add the class of showMainNav to the id mainNav
         mainNav.classList.add("showMainNav");
-        visible = true
+
+        signInContainer.classList.add("showSignIn");
+        canYouSeeMainNav = true;
+    }else{
+        console.log("hide menu");
+        // this will remove the class of showMainNav to the id mainNav
+        mainNav.classList.remove("showMainNav");
+
+        signInContainer.classList.remove("showSignIn");
+
+        canYouSeeMainNav = false;
     }
     
 }
+
+var signUpModule = document.querySelector("#sign-up-container");
+
+function closeModal(){
+    signUpModule.classList.remove("show-signUp-container");
+}
+
+function showModal() {
+    signUpModule.classList.add("show-signUp-container");
+    mainNav.classList.remove("showMainNav");
+ }
+
